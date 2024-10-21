7feeea31991f1663317810d0dd3d4e714836d38a
Notsayy <jtertre25@gmail.com>
2024-10-21 15:10:00 +0200

    Print a program that will assign a random number to the variable

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Description: Prints a specific message to stdout using puts.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d \n", x);
+
+if (x > 0)
+{
+printf("The number is positive \n");
+}
+else if (x < 0)
+{
+printf("The number is negative \n");
+}
+else
+{
+printf("The number is zero \n");
+}
+return (0);
+}
+
