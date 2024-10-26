b77be297649d1964c1e41d19bdb8bf165f1a9cc9
Notsayy <jtertre25@gmail.com>
2024-10-26 14:46:50 +0200

    Write a function that prints the 9 times table, starting with 0.

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * times_table - imprime la table de multiplication jusqu'à 9.
+ * times_table - prints the multiplication table up to 9.
  */
 void times_table(void)
 {
-	int ligne, colonne, produit;
+int ligne, colonne, produit;
 
-	for (ligne = 0; ligne <= 9; ligne++)
-	{
-		_putchar('0');
-		for (colonne = 1; colonne <= 9; colonne++)
-		{
-			_putchar(',');
-			_putchar(' ');
+for (ligne = 0; ligne <= 9; ligne++)
+{
+_putchar('0');
+for (colonne = 1; colonne <= 9; colonne++)
+{
+_putchar(',');
+_putchar(' ');
 
-			produit = ligne * colonne;
+produit = ligne * colonne;
 
-			if (produit <= 9)
-				_putchar(' ');
-			else
-				_putchar((produit / 10) + '0');
-			_putchar((produit % 10) + '0');
-		}
-		_putchar('\n');
-	}
+if (produit <= 9)
+_putchar(' ');
+else
+_putchar((produit / 10) + '0');
+_putchar((produit % 10) + '0');
+}
+_putchar('\n');
+}
 }
