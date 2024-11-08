ce05ce7cc99445924db77360981fde1c7d540565
Notsayy <jtertre25@gmail.com>
2024-11-08 19:21:50 +0100

    idk

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,8 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
  * main - This function is main
  * @argc: arguments contents
@@ -9,20 +11,19 @@
  */
 
 int main(int argc, char *argv[])
-
 {
 	int result = 0;
 	int i, j;
-
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (1);
 	}
+
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; (argv[i][j]); j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
@@ -30,9 +31,7 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-
 		result += atoi(argv[i]);
-
 	}
 	printf("%d\n", result);
 	return (0);
