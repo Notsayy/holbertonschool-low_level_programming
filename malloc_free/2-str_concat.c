4b702a2f4e2ef07fa61142d6a813d88277c5c103
Notsayy <jtertre25@gmail.com>
2024-11-13 18:05:36 +0100

    Write a function that concatenates two strings.

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,34 +10,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *ptr;
-unsigned int len1 = 0;
-unsigned int len2 = 0;
-unsigned int i = 0;
-unsigned int j = 0;
+	char *ptr;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-len1 = strlen(s1);
-len2 = strlen(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-
 	if (ptr == NULL)
 		return (NULL);
 
-	while (i < len1)
+	for (unsigned int i = 0; i < len1; i++)
 	{
-	ptr[i] = s1[i];
-	i++;
+		ptr[i] = s1[i];
 	}
-	while (j < len2)
+	for (unsigned int j = 0; j < len2; j++)
 	{
-	ptr[len1 + j] = s2[j];
-	j++;
+		ptr[len1 + j] = s2[j];
 	}
 	ptr[len1 + len2] = '\0';
 
