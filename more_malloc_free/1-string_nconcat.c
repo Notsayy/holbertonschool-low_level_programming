cea61e07a1e443aaf239946c2ec29d115df64027
Notsayy <jtertre25@gmail.com>
2024-11-14 14:54:08 +0100

    Write a function that concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,47 +2,44 @@
 #include "main.h"
 
 /**
- * string_nconcat - function that concatenates two strings
- * @s1: string 1
- * @s2: ddd
- * @n: dd
- * Return: pointer to the new concatenated string, or NULL on failure
- * If NULL is passed, treat it as an empty string
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes from s2 to concatenate
+ *
+ * Return: pointer to newly allocated space in memory, or NULL if it fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int k;
 	char *ptr;
-	unsigned int j = 0;
+	unsigned int i, j;
+	unsigned int k = 0;
+	unsigned int l = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	if (n >= j)
-		n = j;
+	while (s1[k])
+		k++;
+	while (s2[l])
+		l++;
 
-	ptr = malloc(sizeof(char) * (i + n + 1));
+	if (n >= l)
+		n = l;
+
+	ptr = malloc(sizeof(char) * (k + n + 1));
 	if (ptr == NULL)
 		return (NULL);
 
-	for (k = 0; k < i; k++)
-	{
-		ptr[k] = s1[k];
-	}
+	for (i = 0; i < k; i++)
+		ptr[i] = s1[i];
 
-	for (k = 0; k < j; k++)
-	{
-		ptr[i + k] = s2[k];
-	}
+	for (j = 0; j < n; j++, i++)
+		ptr[i] = s2[j];
 
-	ptr[i + n] = '\0';
+	ptr[i] = '\0';
 
 	return (ptr);
 }
