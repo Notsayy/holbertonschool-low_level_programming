f121c7ab290dca2ec1472a4be655eb845ae40527
Notsayy <jtertre25@gmail.com>
2024-11-14 14:59:20 +0100

    Write a function that allocates memory using malloc.

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,12 @@
 #include "main.h"
 
 /**
- * malloc_checked - function that concatenates two strings
- * @b: string 1
- * Return: pointer to the new concatenated string, or NULL on failure
- * If NULL is passed, treat it as an empty string
+ * malloc_checked - function that allocates memory
+ * @b: int for memory
+ * Return: a pointer to the allocated memory
+ * If fails, function should cause normal termination with a status of 98
  */
+
 
 void *malloc_checked(unsigned int b)
 {
