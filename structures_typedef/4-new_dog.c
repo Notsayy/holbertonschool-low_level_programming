2d3683fb3246c8e1dcfc45b2a7e4ce861d858056
Notsayy <jtertre25@gmail.com>
2024-11-15 15:33:49 +0100

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,66 +9,40 @@
  * @age: age of dog
  * Return: pointer to new_dog or NULL if it fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	unsigned int name_length = 0;
-	unsigned int owner_length = 0;
-	char *name_copy;
-	char *owner_copy;
-	unsigned int i;
+	unsigned int name_length = 0, owner_length = 0, i;
 
-	if (name != NULL && owner != NULL)
-{
-	new_dog = malloc(sizeof(dog_t));
-		if (new_dog == NULL)
-			return (NULL);
-
-	while (name[name_length] != '\0')
-	{
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	while (name[name_length])
 		name_length++;
-	}
-
-	while (owner[owner_length] != '\0')
-	{
+	while (owner[owner_length])
 		owner_length++;
-	}
-
-	
-
-	name_copy = malloc(name_length + 1);
-	if (name_copy == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = malloc(name_length + 1);
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < name_length; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[name_length] = '\0';
+	new_dog->owner = malloc(owner_length + 1);
+	if (new_dog->owner == NULL)
 	{
-		name_copy[i] = name[i];
-	}
-
-	name_copy[name_length] = '\0';
-
-	owner_copy = malloc(owner_length + 1);
-	if (owner_copy == NULL)
-	{
-		free(name_copy);
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 
 	for (i = 0; i < owner_length; i++)
-	{
-		owner_copy[i] = owner[i];
-	}
-
-	owner_copy[owner_length] = '\0';
-
-	new_dog->name = name_copy;
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[owner_length] = '\0';
 	new_dog->age = age;
-	new_dog->owner = owner_copy;
-}	
 	return (new_dog);
 }
