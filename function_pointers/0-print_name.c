ae1cc615cd0e28dc44e9e4f68601c4493046c5fe
Notsayy <jtertre25@gmail.com>
2024-11-18 11:51:12 +0100

    conflict

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name using a given function
+ * @name: Name to be printed
+ * @f: Pointer to the printing function
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *)
+{
+
+
+}
