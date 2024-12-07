d0f80a6e16a816da98926541d0ea8c3830e149a1
Notsayy <jtertre25@gmail.com>
2024-12-07 12:26:47 +0100

    Updated my 3-cp.c for the checker

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,28 +5,21 @@
 
 #define BUFFER_SIZE 1024
 
-char *create_buffer(char *file);
-
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
+ *
  * Return: A pointer to the newly-allocated buffer.
  */
-
 char *create_buffer(char *file)
-
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't allocate memory for %s\n", file);
 		exit(99);
 	}
-
 	return (buffer);
 }
 
@@ -59,15 +52,18 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to\n", "");
 
+	buffer = create_buffer(argv[2]);
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
+	{
+		close(fd_from);
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-
-	buffer = create_buffer(argv[2]);
+	}
 
 	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -79,13 +75,9 @@ int main(int argc, char *argv[])
 	if (rd == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", (void *)&fd_from);
-
-	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", (void *)&fd_to);
-
 	free(buffer);
+	close(fd_from);
+	close(fd_to);
 
 	return (0);
 }
